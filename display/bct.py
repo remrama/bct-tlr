bece3f9b753a9934fc0481f29fefa2fd8086cba8
remrama <mallett.remy@gmail.com>
2022-06-27 19:47:32 -0500

    drop stupid mouse

--- display/bct.py
+++ display/bct.py
@@ -19,7 +19,6 @@ class BreathCountingTask(Game):
         session_number,
         acquisition_code,
         room_number,
-        use_handheld_mouse,
         task_name="bct",
         target_digit=9, # number of presses!
         min_press_gap=1, # minimum seconds between presses that triggers warning in practice
@@ -34,27 +33,29 @@ class BreathCountingTask(Game):
         self.min_press_gap = min_press_gap
 
         self.taskClock = core.Clock() # gets reset when real task starts
+        self.trialClock = core.Clock() # for RT warnings during practice
 
         self.min_opacity = .1 # for the text/arrows during practice
 
-        if use_handheld_mouse:
-            self.response_legend = {
-                0: "nontarget",
-                1: "reset",
-                2: "target",
-            }
-        else:
-            self.response_legend = {
-                0: "nontarget",
-                1: "reset",
-                2: "target",
-            }
+        # if use_mouse:
+        #     self.response_legend = {
+        #         0: "nontarget",
+        #         1: "reset",
+        #         2: "target",
+        #     }
+        # else:
+        self.response_legend = {
+            "left": "nontarget",
+            "right": "reset",
+            "space": "target",
+        }
+        self.keylist = list(self.response_legend.keys())
 
         self.header_text = {
             "fast": "That breath was too fast.",
-            "early": f"Don't press the Top button until count {self.target_digit}.\n\nLet's restart the counter.",
-            "late": f"Remember to press the Top button on count {self.target_digit}.\n\nThe count will not reset until the Top button or the Scroll Wheel is pressed.",
-            "instructions": f"Press the Trigger button with breaths 1-{self.pretarget_digit} and a Top button with breath {self.target_digit}.\n\nPress the Scroll Wheel and restart the count at 1 if you lose track.",
+            "early": f"Don't press the Right Arrow until count {self.target_digit}.\n\nLet's restart the counter.",
+            "late": f"Remember to press the Right Arrow on count {self.target_digit}.\n\nThe count will not reset until the Right Arrow or the Spacebar is pressed.",
+            "instructions": f"Press the Left Arrow with breaths 1-{self.pretarget_digit} and a Right Arrow with breath {self.target_digit}.\n\nPress the Spacebar and restart the count at 1 if you lose track.",
         }
 
         self.instructions_messages = [
@@ -62,7 +63,7 @@ class BreathCountingTask(Game):
             "At some point, you may notice your attention has wandered from the breath.\n\nThat's okay. Just gently place it back on the breath.",
             f"To help attention stay with the breath, use part of your attention to silently count breaths from 1 to {self.target_digit} again and again.",
             "Say the count softly in your mind (not out loud or with fingers) while most of the attention is on feeling the breath.",
-            f"During counting, press a button with each breath.\n\nPress the Trigger button on breaths 1-{self.pretarget_digit} and either of the Top buttons on breath {self.target_digit}.\n\nIf you find that you have forgotten the count, just press down on the Scroll Wheel and restart the count at 1 with the next breath.",
+            f"During counting, press a button with each breath.\n\nPress the Left Arrow on breaths 1-{self.pretarget_digit} and the Right Arrow on breath {self.target_digit}.\n\nIf you find that you have forgotten the count, just press the Spacebar and restart your count at 1 with the next breath.",
             f"Sit in an upright, relaxed posture that feels comfortable.",
         ]
 
@@ -157,6 +158,43 @@ class BreathCountingTask(Game):
             self.digitsTexts[current_index].setOpacity(1)
         self.topText.text = self.header_text[text_key]
         self.topText.setColor(text_color)
+        self.win.flip()
+
+    # def collect_response_mouse(self):
+    #     """Wait for a response and record it.
+    #     Return keypress if a valid press.
+    #     Return None if experiment is over.
+    #     Quit if manual exit (push quit button).
+    #     """
+    #     self.fixationStim.draw() # could autodraw
+    #     self.win.flip()
+    #     event.clearEvents("mouse")
+    #     self.mouse.clickReset()
+    #     while not self.task_ended: # returns out after press
+    #         presses, timestamps = self.mouse.getPressed(getTime=True)
+    #         if sum(presses) == 1: # a button was clicked
+    #             self.flutter_fixation()
+    #             clicked_index = presses.index(True)
+    #             rt = timestamps[clicked_index]
+    #             response = self.response_legend[clicked_index]
+    #             # left_clicked, right_clicked, _ = presses
+    #             # left_rt, right_rt, _ = timestamps
+    #             if not self.passed_practice:
+    #                 txt_key = "instructions"
+    #                 if response == "target" and self.breath_counter < self.target_digit:
+    #                     txt_key = "early"
+    #                 elif response == "nontarget" and self.breath_counter == self.target_digit:
+    #                     txt_key = "late"
+    #                 elif self.cycle_responses:
+    #                     if rt < self.min_press_gap:
+    #                         txt_key = "fast"
+    #                 self.change_practice_screen(text_key=txt_key)
+    #             return response
+
+    #         # check for reasons to exit
+    #         self.check_for_quit()
+    #         if self.passed_practice and self.taskClock.getTime() > self.task_length:
+    #             self.task_ended = True
 
     def collect_response(self):
         """Wait for a response and record it.
@@ -166,28 +204,27 @@ class BreathCountingTask(Game):
         """
         self.fixationStim.draw() # could autodraw
         self.win.flip()
-        event.clearEvents("mouse")
-        self.mouse.clickReset()
+        self.trialClock.reset()
+        event.clearEvents(eventType="keyboard")
         while not self.task_ended: # returns out after press
-            presses, timestamps = self.mouse.getPressed(getTime=True)
-            if sum(presses) == 1: # a button was clicked
+            for response in event.getKeys(keyList=self.keylist, timeStamped=self.trialClock):
                 self.flutter_fixation()
-                clicked_index = presses.index(True)
-                rt = timestamps[clicked_index]
-                response = self.response_legend[clicked_index]
+                key, rt = response
+                print(key)
+                response_type = self.response_legend[key]
                 # left_clicked, right_clicked, _ = presses
                 # left_rt, right_rt, _ = timestamps
                 if not self.passed_practice:
                     txt_key = "instructions"
-                    if response == "target" and self.breath_counter < self.target_digit:
+                    if response_type == "target" and self.breath_counter < self.target_digit:
                         txt_key = "early"
-                    elif response == "nontarget" and self.breath_counter == self.target_digit:
+                    elif response_type == "nontarget" and self.breath_counter == self.target_digit:
                         txt_key = "late"
                     elif self.cycle_responses:
                         if rt < self.min_press_gap:
                             txt_key = "fast"
                     self.change_practice_screen(text_key=txt_key)
-                return response
+                return response_type
 
             # check for reasons to exit
             self.check_for_quit()
@@ -273,7 +310,7 @@ class BreathCountingTask(Game):
     def task(self):
         self.audioStim.stop()
         self.show_message_and_wait_for_press(self.pretask_message)
-        core.wait(1) # just to clear to mouse is lifted before starting
+        core.wait(1) # just to clear so mouse is lifted before starting
         self.audioStim.play()
         self.send_slack_notification("Task started")
         self.send_to_pport(self.pport_codes["bct-start"])
@@ -335,14 +372,12 @@ if __name__ == "__main__":
     parser.add_argument("--session", type=int, default=1)
     parser.add_argument("--acq", type=str, default="pre", choices=["pre", "post"])
     parser.add_argument("--room", type=int, default=207, choices=[0, 207])
-    parser.add_argument("--handheld", action="store_true", help="Use handheld mouse.")
     args = parser.parse_args()
 
     subject_number = args.subject
     session_number = args.session
     acquisition_code = args.acq
     room_number = args.room
-    use_handheld_mouse = args.handheld
 
-    bct = BreathCountingTask(subject_number, session_number, acquisition_code, room_number, use_handheld_mouse)
+    bct = BreathCountingTask(subject_number, session_number, acquisition_code, room_number)
     bct.run()
